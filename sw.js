f0df5cee4445ca4b2d3b50ee76eb146fdf86296b
gabriele1211 <gabriele121127@gmail.com>
2026-08-10 17:16:33 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="grandine-alert-version-1-1-18-radar-sync";
-const APP_SHELL=["./manifest.webmanifest","./assets/index-v1.1.18.js","./assets/index-v1.1.18.css","./assets/leaflet-v1.1.18.js","./assets/map-label-fix.css?v=1.1.18","./assets/countries-110m.geojson","./assets/gf-logo.png","./icons/grandine-icon-192.png","./icons/grandine-icon-512.png"];
+const CACHE="grandine-alert-version-1-1-19-radar-cells";
+const APP_SHELL=["./manifest.webmanifest","./assets/index-v1.1.19.js","./assets/index-v1.1.19.css","./assets/leaflet-v1.1.19.js","./assets/map-label-fix.css?v=1.1.19","./assets/countries-110m.geojson","./assets/gf-logo.png","./icons/grandine-icon-192.png","./icons/grandine-icon-512.png"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)));self.skipWaiting()});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith("grandine-alert-")&&key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim()});
 self.addEventListener("fetch",event=>{
